c04b12a003cef0b6cb37639094851bcd38035998
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-09-17 14:07:00 -0300

    fix(backend): remove redundant and strict validation

--- backend-app-joyas/middlewares/validatePagination.js
+++ backend-app-joyas/middlewares/validatePagination.js
@@ -1,32 +1,6 @@
-const validateOrderParam = ( field, direction ) => {
-
-    const fieldList = [
-        "id",
-        "nombre",
-        "categoria",
-        "metal",
-        "precio",
-        "stock"
-    ];
-
-    const directionList = [ "ASC", "DESC" ];
-
-    const safeField =
-        fieldList.includes( field?.toLowerCase() )
-        ? field.toLowerCase()
-        : undefined;
-
-    const safeDirection =
-        directionList.includes( direction?.toUpperCase() )
-        ? direction.toUpperCase()
-        : undefined;
-
-    return { safeField, safeDirection };
-};
-
 export const validatePagination = ( req, res, next ) => {
 
-    const { page, limits, order_by } = req.query;
+    const { page, limits } = req.query;
     
     const isPosInt = /^[1-9]\d*$/;
 
@@ -42,24 +16,6 @@ export const validatePagination = ( req, res, next ) => {
         error.status = 400;
 
         return next(error);
-    };
-    
-    if ( order_by ) {
-        const [ field, direction ] = order_by.split( "_" );
-        const { safeField, safeDirection } = validateOrderParam( field, direction );
-
-        if ( !safeField || !safeDirection ) {
-            const error = new Error(
-                `Bad Request. Invalid order_by query param statement.
-                Use [ id | nombre | categoria | metal | precio | stock ]
-                and [ ASC | DESC ] instead`
-            );
-            error.status = 400;
-
-            return next(error);
-        };
-        
-        return next();
     };
 
     next();
